a4a1847975091f05527d31bb69bb944a3024d08e
caugust1 <106704933+caugust1@users.noreply.github.com>
2026-06-15 09:32:43 +0200

    Update group12.m
    Added circuit component values that were given to us.  Added equation for the shunt resistor which can be solved after performing the modal analysis of the structure.

--- group12.m
+++ group12.m
@@ -23,3 +23,23 @@ end
 
 % We now have n repitition of the random signal we gave
 % Problem: if the signal is random, we can't average to take out the noise
+
+%% Solving for Circuit Component Values - Targeting Mode 1
+% Values supplied to us:
+R2 = 10E3; % Ohms
+Ccap = 40E-9; % Farad, C,hat in NC circuit diagram.
+Rcap = 0.9E6; % Ohms, R,hat in NC circuit diagram.
+Beta = 0.7;
+Cp1 = 40E-9; % Farad, mode 1 equivalent capacitance - we may get an updated value once we arrive.
+Req = -50E6; % Ohms, equivalent resistance of Rs and R~ in parallel.
+
+% Need R1, Rsh, and Rs.
+C2 = Cp1/Beta;  % Capacitance of NC circuit. (Eq. 37)
+R1 = (R2*Ccap)/C2;  % R1 from NC circuit. (Eq. 54)
+Rtilda = R1*Rcap/R2;  % Ohms, R~ (Eq. 55)
+Rs = Rtilda*Req/(Req + Rtilda);  % Ohms, balancing resistor. (Eq. 59 rearranged)
+
+omega1 = "?";  % Open Circuit Nat. Freq. Mode 1: Needs to be solved for from modal analysis.
+omegaHat1 = "?";  % Closed Circuit Nat. Freq. Mode 1: Needs to be solved for from modal analysis.
+k1 = sqrt( omegaHat1^2/omega1^2 - 1 );  % Mode 1 coupling factor.
+Rsh = (1-Beta2)/(omega1*Cp1) * sqrt(2/( k1^2/(1-Beta) + 2 -2*k1*sqrt(Beta/(1-Beta) ) ) );
